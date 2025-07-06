18a96943303ad4cb0cf6ba8ae90ed1850ec92194
harshal-09-p <harshal.phadatare@gmail.com>
2025-07-06 21:08:05 +0530

    puh raw data

---
+++ github_issues.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get(f"https://api.github.com/repos/Harshal-HP/python-github/issues")
+
+if response.status_code == 200:
+    response_data = response.json()
+    for data in response_data.items():
+        print(data)
